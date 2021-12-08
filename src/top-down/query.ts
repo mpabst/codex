8e76389c83d43d6ad79837453c005a3bdee02cc9
Michael Pabst <michael.pabst@cs.stanford.edu>
2021-12-07 22:23:08 -0600

    It's ~as fast un-inlined

--- src/top-down/query.ts
+++ src/top-down/query.ts
@@ -1,6 +1,6 @@
 import { Term, Variable } from '../term.js'
 import { Branch, Twig, Node, Store } from '../collections/store.js'
-import { Statement } from './syntax.js'
+import { Pattern, Statement } from './syntax.js'
 
 export type Bindings = Map<Variable, Term>
 
@@ -19,6 +19,47 @@ export function evaluate(
   ) {
     let op: Operation, term: Term, value: Term | undefined
 
+    function doLine(): boolean {
+      op = op as Pattern
+      if (termIndex === 0) data = store[op.order]
+      for (; termIndex < op.pattern.length; termIndex++) {
+        term = op.pattern[termIndex]
+        value = term.termType === 'Variable'
+          ? bindings.get(term as Variable)
+          : term
+        if (termIndex === op.pattern.length - 1) {
+          if (!doTwig()) return false
+        } else if (!doBranch()) return false
+      }
+      termIndex = 0
+      return true
+    }
+
+    function doTwig(): boolean {
+      data = data as Twig
+      if (value) return data.has(value)
+      for (const t of data) {
+        bindings.set(term as Variable, t)
+        choose([...stack])
+      }
+      bindings.delete(term as Variable)
+      return false
+    }
+
+    function doBranch(): boolean {
+      data = data as Branch
+      if (value) {
+        data = data.get(value)!
+        return !!data
+      }
+      for (const [k, v] of data.entries()) {
+        bindings.set(term as Variable, k)
+        choose([...stack, op], v, termIndex + 1)
+      }
+      bindings.delete(term as Variable)
+      return false
+    }
+
     while (true) {
       op = stack.pop()!
 
@@ -31,45 +72,8 @@ export function evaluate(
 
       switch (op.type) {
         case 'Pattern':
-          if (termIndex === 0) data = store[op.order]
-          for (; termIndex < op.pattern.length; termIndex++) {
-            term = op.pattern[termIndex]
-            value = term.termType === 'Variable'
-              ? bindings.get(term as Variable)
-              : term
-
-            if (termIndex === op.pattern.length - 1) {
-              data = data as Twig
-              if (value) {
-                if (data.has(value)) continue
-                else return
-              }
-              for (const t of data) {
-                bindings.set(term as Variable, t)
-                choose([...stack])
-              }
-              bindings.delete(term as Variable)
-              return
-
-            } else {
-              data = data as Branch
-              if (value) {
-                data = data.get(value)!
-                if (!data) return
-                continue
-              }
-              for (const [k, v] of data.entries()) {
-                bindings.set(term as Variable, k)
-                choose([...stack, op], v, termIndex + 1)
-              }
-              bindings.delete(term as Variable)
-              return
-            }
-          }
-
-          termIndex = 0
+          if (!doLine()) return
           continue
-
         case 'Conjunction':
           stack.push(op.rest, op.first)
           continue

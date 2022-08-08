6ad7e058a0a01f0082b38da1eee7f4dc1bd3db9f
Michael Pabst <michael.pabst@cs.stanford.edu>
2022-08-08 16:14:45 -0500

    Working on backtracking
    About to remove the trail

--- src/machine.ts
+++ src/machine.ts
@@ -10,6 +10,18 @@ type Operation = (m: Query, t: Argument) => void
 type Instruction = [Operation, Argument]
 type Program = Instruction[]
 
+class ChoicePoint {
+  trail: Variable[] = []
+  done: boolean = false
+
+  constructor(
+    public variable: Variable | null,
+    public instructionPtr: number,
+    public clause: Clause | null,
+    public iterator: Iterator<Term> | null,
+  ) {}
+}
+
 class Query {
   program: Program
   bindings: Bindings = new Map()
@@ -18,11 +30,37 @@ class Query {
   args: Bindings = new Map()
   dbNode: Node | null = null
   fail: boolean = false
+  // push a dummy choice point so we don't have to branch when
+  // pushing variables onto the trail
+  stack: ChoicePoint[] = [new ChoicePoint(null, 0, null, null)]
+  emit: ((b: Bindings) => void) | null = null
+  seenVars = new Set<Variable>()
 
   constructor(public hub: Store, source: Expression) {
     const [program, variables] = compile(hub, source)
     this.program = program
     for (const v of variables) this.bindings.set(v, v)
+  }
+
+  backtrack(): boolean {
+    while (true) {
+      if (this.stack.length === 1) return false
+
+      const choicePoint = this.stack[this.stack.length - 1]
+      if (choicePoint.done) {
+        this.stack.pop()
+        continue
+      }
+
+      // unbind trail vars
+      for (const v of choicePoint.trail) this.bindings.set(v, v)
+      choicePoint.trail = []
+
+      this.instructionPtr = choicePoint.instructionPtr
+      this.clause = choicePoint.clause
+
+      return true
+    }
   }
 
   deref(variable: Variable): Term {
@@ -34,13 +72,14 @@ class Query {
     return found
   }
 
-  run(): boolean {
-    let instruction: Instruction
-    while (!this.fail) {
-      instruction = this.program[this.instructionPtr]
+  run(emit: (b: Bindings) => void = console.log): void {
+    this.emit = emit
+    while (true) {
+      const instruction = this.program[this.instructionPtr]
       instruction[0](this, instruction[1])
+      if (this.fail && !this.backtrack()) break
     }
-    return !this.fail
+    this.emit = null
   }
 }
 
@@ -59,37 +98,61 @@ const operations: { [k: string]: Operation } = {
 
   medialConstant(query: Query, term: Argument): void {
     const found = (query.dbNode as Branch).get(term as Term)
-    if (!found) query.fail = true
-    else query.dbNode = found
-
-    // if (found.termType === 'Variable')
-
+    if (!found) {
+      query.fail = true
+      return
+    } else query.dbNode = found
     query.instructionPtr++
   },
 
   medialNewVariable(query: Query, term: Argument): void {
+    let choicePoint: ChoicePoint
 
-  },
+    if (query.seenVars.has(term as Variable)) {
+      choicePoint = query.stack[query.stack.length - 1]
+    } else {
+      choicePoint = new ChoicePoint(
+        term as Variable,
+        query.instructionPtr,
+        query.clause,
+        (query.dbNode as Branch).keys(),
+      )
+      query.stack.push(choicePoint)
+      query.seenVars.add(term as Variable)
+    }
 
-  medialOldVariable(query: Query, term: Argument): void {},
-
-  finalConstant(query: Query, term: Argument): void {
-    query.fail = !(query.dbNode as Twig).has(term as Term)
-
-    // if (found.termType === 'Variable')
+    const result = choicePoint.iterator?.next()!
+    if (result.done) {
+      choicePoint.done = true
+      query.fail = true
+      return
+    } else query.bindings.set(term as Variable, result.value)
 
     query.instructionPtr++
   },
 
-  finalNewVariable(query: Query, term: Argument): void {
-
+  medialOldVariable(query: Query, term: Argument): void {
+    operations.medialConstant(query, query.deref(term as Variable))
   },
 
-  finalOldVariable(query: Query, term: Argument): void {},
+  finalConstant(query: Query, term: Argument): void {
+    if ((query.dbNode as Twig).has(term as Term)) query.instructionPtr++
+    else query.fail = true
+  },
+
+  finalNewVariable(query: Query, term: Argument): void {},
+
+  finalOldVariable(query: Query, term: Argument): void {
+    operations.finalConstant(query, query.deref(term as Variable))
+  },
 
   call(query: Query, term: Argument): void {
     query.clause = null
     query.args = new Map()
+  },
+
+  emitResult(query: Query, term: Argument): void {
+    query.emit!(new Map(query.bindings))
   },
 }
 
@@ -101,7 +164,8 @@ function compile(store: Store, query: Expression): [Program, Set<Variable>] {
     // assume GSPO
     const context = store.get(expr.terms[0] as Key)!
     if (context instanceof Clause)
-      program.push([operations.setClause, context])
+      // program.push([operations.setClause, context])
+      throw new Error('todo: calls')
     else program.push([operations.setIndex, context])
 
     expr.terms.slice(1).forEach((term, i) => {
@@ -119,10 +183,13 @@ function compile(store: Store, query: Expression): [Program, Set<Variable>] {
       ])
     })
 
+    // this is wrong, we only call as we change to a new context
     if (context instanceof Clause) program.push([operations.call, null])
   }
 
   traverse(query, { pattern })
+
+  program.push([operations.emitResult, null])
 
   return [program, variables]
 }

d7087a52ccdd0662b29fe30fdb16adff38a79bec
Michael Pabst <michael.pabst@cs.stanford.edu>
2022-08-08 23:47:58 -0500

    Tidying up

--- src/test/index.d.ts
+++
@@ -1,9 +0,0 @@
-import chai from 'chai'
-
-declare global {
-  interface Window {
-    // TypeScript, or something, will shim Mocha's BDD globals for us, but we
-    // need to do this
-    chai: typeof chai
-  }
-}

adaa45eb6568d3ff1295f9c4742ce5c77cc75fb7
Michael Pabst <michael.pabst@cs.stanford.edu>
2022-08-12 00:39:30 -0500

    Head matching operations, minus varKeys
    Also needs head compilation

--- src/calls.ts
+++ src/calls.ts
@@ -1,108 +1,105 @@
-// call instructions:
-// constant: choose through [correct value] + [...callee vars]
+import { Argument, ChoicePoint, Operation, Query, Side } from './query.js'
+import { Term, Variable } from './term.js'
 
-import { Query } from './query.js'
-import { Term } from './term.js'
-
-function pushChoicePoint(it: Iterator<Term>): ChoicePoint {
-  const out = new ChoicePoint(this.instructionPtr, this.clause, this.dbNode, it)
-  this.stack.push(out)
-  return out
-}
-
-function inferredConstant(
+function iTerm(
   query: Query,
   term: Argument,
-  advanceNode: Operation,
-  declaredConstant: Operation,
+  doConst: Operation,
+  doVar: (cp: ChoicePoint, r: IteratorResult<Variable>) => void,
 ): void {
-  if (query.dbNode!.varKeys.size > 0) {
-    declaredConstant(query, term)
+  const options = query.dbNode!.varKeys
+
+  if (options.size === 0) {
+    doConst(query, term)
     return
   }
 
-  let choicePoint = query.stack[query.stack.length - 1]
-  if (!choicePoint || choicePoint.instructionPtr !== query.instructionPtr)
-    choicePoint = query.pushChoicePoint(query.dbNode!.varKeys.values())
-
-  const result = choicePoint.iterator?.next()!
-  if (result.done) {
-    query.calleeBindings.set(
-      choicePoint.calleeBinding,
-      choicePoint.calleeBinding,
-    )
-    query.stack.pop()
-    declaredConstant(query, term)
-  } else {
-    const ultimate = deref(query.calleeBindings, result.value)
-    if (ultimate.termType === 'Variable') {
-      query.calleeBindings.set(ultimate, term)
-      choicePoint.calleeBinding = ultimate
-      advanceNode(query, result.value)
-      query.instructionPtr++
-    } else if (ultimate === term) query.instructionPtr++
-    else query.fail = true
+  const cp = query.peek(options)
+  if (cp.trail) {
+    if (cp.side === Side.Caller) query.scope.set(cp.trail, cp.trail)
+    else query.calling.delete(cp.trail)
   }
+
+  const result = (cp.iterator as Iterator<Variable>).next()
+  if (result.done) {
+    query.stack.pop()
+    doConst(query, term)
+    return
+  }
+
+  doVar(cp, result)
 }
 
-function inferredNewVariable(query: Query, term: Argument) {
-  let choicePoint = query.stack[query.stack.length - 1]
-
-  if (!choicePoint || choicePoint.instructionPtr !== query.instructionPtr)
-    choicePoint = query.pushChoicePoint(query.dbNode!.keys())
-
-  if (choicePoint.calleeBinding)
-    query.calleeBindings.set(
-      choicePoint.calleeBinding,
-      choicePoint.calleeBinding,
-    )
-
-  const result = choicePoint.iterator?.next()!
-
-  if (result.done) {
-    query.bindings.set(term as Variable, term as Term)
-    query.stack.pop()
-    query.fail = true
-    return
+function iConst(
+  query: Query,
+  term: Argument,
+  advance: Operation,
+  eConst: Operation,
+): void {
+  function doVar(cp: ChoicePoint, result: IteratorResult<Variable>) {
+    const proxi = query.calling.get(result.value as Variable)
+    if (!proxi) {
+      query.calling.set(result.value, term as Term)
+      cp.trail = result.value
+      cp.side = Side.Callee
+    } else if (proxi.termType !== 'Variable') {
+      if (proxi !== term) {
+        query.fail = true
+        return
+      }
+    } else {
+      const ulti = query.deref(proxi as Variable)
+      if (ulti.termType === 'Variable') {
+        query.scope.set(ulti as Variable, term as Term)
+        cp.trail = ulti as Variable
+        cp.side = Side.Caller
+      } else if (ulti !== term) {
+        query.fail = true
+        return
+      }
+    }
+    advance(query, result.value)
   }
-
-  query.bindings.set(term as Variable, result.value)
-  if (result.value.termType === 'Variable') {
-    const ultimate = deref(query.calleeBindings, result.value)
-    if (ultimate.termType === 'Variable') {
-      query.calleeBindings.set(ultimate, term)
-      choicePoint.calleeBinding = ultimate
-      query.bindings.set(term, result.value)
-      advanceNode(query, result.value)
-      query.instructionPtr++
-    } else if (ultimate === term) {
-      advanceNode(query, result.value)
-      query.instructionPtr++
-    } else query.fail = true
-    return
-  }
-
-  query.bindings.set(term, result.value)
-  advanceNode(query, result.value)
-  query.instructionPtr++
+  iTerm(query, term, eConst, doVar)
 }
 
-function inferredOldVariable(query: Query, term: Argument): void {}
+function iNewVar(
+  query: Query,
+  term: Argument,
+  advance: Operation,
+  eNewVar: Operation,
+) {
+  function doVar(cp: ChoicePoint, result: IteratorResult<Variable>) {
+    let found = query.calling.get(result.value as Variable)
+    if (!found) {
+      query.calling.set(result.value, term as Term)
+      cp.trail = result.value
+      cp.side = Side.Callee
+    } else {
+      if (found.termType === 'Variable') found = query.deref(found as Variable)
+      query.scope.set(term as Variable, found)
+      cp.trail = term as Variable
+      cp.side = Side.Caller
+    }
+    advance(query, result.value)
+  }
+  iTerm(query, term, eNewVar, doVar)
+}
 
-// new variable: choose through all callee values
-// seen var: as constant, with deref
-
-// caller vars: bind as usu, to proximal deref
-// ditto arg mapping, at call time, do full deref
+function iOldVar(
+  query: Query,
+  term: Argument,
+  advance: Operation,
+  eNewVar: Operation,
+  eConst: Operation,
+): void {
+  const found = query.deref(term as Variable)
+  if (found.termType === 'Variable') iNewVar(query, found, advance, eNewVar)
+  else iConst(query, found, advance, eConst)
+}
 
 // call:
-// deref args (copy), Query.evaluate(), collect result bindings, choose through, mapping back to caller vars
-
-// backtracking needs to undo callee arg mapping
-
-// we need the trail back, too, because with collecting calls, we'll need to undo more than one
-// var on backtracking
-
-// push onto array on CP
-
-// second array for callee arg mappings. it can just be a scalar.
+// 1. copy list of keys bound to unbound caller var -
+// 2. do call, collect these out-args as caller var -> value
+// 3. push choicepoint, iterate
+// 4. at end of CP, unbind keys from 1, backtrack
--- src/query.ts
+++ src/query.ts
@@ -5,27 +5,40 @@ import { Context, Store } from './store.js'
 import { Expression } from './syntax.js'
 import { Term, Variable } from './term.js'
 
-export type Bindings = Map<Variable, Term>
-type Argument = Term | Context | null
-type Operation = (m: Query, t: Argument) => void
+export type Bindings<T extends Term = Term> = Map<Variable, T>
+type VarMap = Bindings<Variable>
+export type Argument = Term | Context | null
+export type Operation = (m: Query, t: Argument) => void
 type Instruction = [Operation, Argument]
 export type Program = Instruction[]
 
-class ChoicePoint {
-  public instructionPtr: number
+type Keyable = { keys: () => Iterator<Term> }
+
+export enum Side {
+  Caller,
+  Callee,
+}
+
+export class ChoicePoint {
+  public pc: number
   public clause: Clause | null
   public dbNode: Node | null
+  public trail: Variable | null = null
+  public side: Side | null = null
 
-  constructor(query: Query, public iterator: Iterator<Term> | null) {
-    this.instructionPtr = query.instructionPtr
+  constructor(query: Query, public iterator: Iterator<Term>) {
+    this.pc = query.pc
     this.clause = query.clause
     this.dbNode = query.dbNode
   }
 }
 
 export class Query {
+  incoming: VarMap
+  returning: VarMap = new Map()
+
   program: Program
-  instructionPtr: number = 0
+  pc: number = 0
 
   dbNode: Node | null = null
   // instead of failing, just jump to whatever's on top of the stack?
@@ -33,24 +46,24 @@ export class Query {
   // maybe: set fail, fetch next instr, if 'not', then continue, else backtrack
   fail: boolean = false
 
-  bindings: Bindings = new Map()
+  scope: Bindings = new Map()
   stack: ChoicePoint[] = []
 
   clause: Clause | null = null
-  args: Bindings = new Map()
+  // just reach into clause.scope?
+  calling: Bindings = new Map()
 
   emit: ((b: Bindings) => void) | null = null
 
-  incoming: Map<Variable, Variable>
-  outgoing = new Map<Variable, Variable>()
+  iFound: Term | null = null
 
   constructor(public store: Store, source: Expression) {
     const [program, variables] = compile(store, source)
     this.program = program
     this.incoming = variables
     for (const [k, v] of variables) {
-      this.outgoing.set(v, k)
-      this.bindings.set(v, v)
+      this.returning.set(v, k)
+      this.scope.set(v, v)
     }
   }
 
@@ -58,7 +71,7 @@ export class Query {
     const length = this.stack.length
     if (length === 0) return false
     const choicePoint = this.stack[length - 1]
-    this.instructionPtr = choicePoint.instructionPtr
+    this.pc = choicePoint.pc
     this.clause = choicePoint.clause
     this.dbNode = choicePoint.dbNode
     this.fail = false
@@ -66,71 +79,77 @@ export class Query {
   }
 
   deref(variable: Variable): Term {
-    let found = this.bindings.get(variable)!
-    while (found.termType === 'Variable' && found !== variable) {
-      variable = found as Variable
-      found = this.bindings.get(variable)!
-    }
-    return found
+    const found = this.scope.get(variable)!
+    if (found.termType === 'Variable')
+      return found === variable ? found : this.derefCalling(found as Variable)
+    else return found
+  }
+
+  derefCalling(variable: Variable): Term {
+    const found = this.calling.get(variable)!
+    return found.termType === 'Variable' ? this.deref(found as Variable) : found
   }
 
   evaluate(
     emit: (b: Bindings) => void = console.log,
-    bindings: Bindings = new Map(),
+    args: Bindings = new Map(),
   ): void {
     this.emit = emit
-    for (const [k, v] of bindings) this.bindings.set(k, v)
+    for (const [k, v] of args) this.scope.set(k, v)
     while (true) {
-      const instruction = this.program[this.instructionPtr]
+      const instruction = this.program[this.pc]
       instruction[0](this, instruction[1])
       if (this.fail && !this.backtrack()) break
     }
-    for (const k of bindings.keys()) this.bindings.set(k, k)
+
+    // only unbind vars which were unbound at start of eval
+    for (const k of args.keys()) this.scope.set(k, k)
+
     this.emit = null
   }
 
-  pushChoicePoint(it: Iterator<Term>): ChoicePoint {
-    const out = new ChoicePoint(this, it)
-    this.stack.push(out)
+  next(it: Keyable): IteratorResult<Term> {
+    return this.peek(it).iterator?.next()!
+  }
+
+  peek(it: Keyable): ChoicePoint {
+    let out = this.stack[this.stack.length - 1]
+    if (!out || out.pc !== this.pc) {
+      out = new ChoicePoint(this, it.keys())
+      this.stack.push(out)
+    }
     return out
   }
-}
 
-function getNext(query: Query, it: Iterator<Term>): IteratorResult<Term> {
-  let choicePoint = query.stack[query.stack.length - 1]
-  if (!choicePoint || choicePoint.instructionPtr !== query.instructionPtr)
-    choicePoint = query.pushChoicePoint(it)
-  return choicePoint.iterator?.next()!
+  pop(): void {
+    this.stack.pop()
+    this.fail = true
+  }
 }
 
 function advanceMedial(query: Query, term: Argument): void {
   query.dbNode = (query.dbNode as Branch).get(term as Term)!
-  query.instructionPtr++
+  query.pc++
 }
 
 function advanceFinal(query: Query, term: Argument): void {
   query.dbNode = null
-  query.instructionPtr++
+  query.pc++
 }
 
-function noMore(query: Query): void {
-  query.stack.pop()
-  query.fail = true
-}
-
-function anonEVar(query: Query, advance: Operation): void {
-  const result = getNext(query, query.dbNode!.keys())
-  if (result.done) noMore(query)
+function eAnonVar(query: Query, advance: Operation): void {
+  const result = query.next(query.dbNode!)
+  if (result.done) query.pop()
   else advance(query, result.value)
 }
 
-function newEVar(query: Query, term: Argument, advance: Operation): void {
-  const result = getNext(query, query.dbNode!.keys())
+function eNewVar(query: Query, term: Argument, advance: Operation): void {
+  const result = query.next(query.dbNode!)
   if (result.done) {
-    query.bindings.set(term as Variable, term as Term)
-    noMore(query)
+    query.scope.set(term as Variable, term as Term)
+    query.pop()
   } else {
-    query.bindings.set(term as Variable, result.value)
+    query.scope.set(term as Variable, result.value)
     advance(query, result.value)
   }
 }
@@ -139,54 +158,58 @@ export const operations: { [k: string]: Operation } = {
   setClause(query: Query, clause: Argument): void {
     query.clause = clause as Clause
     query.dbNode = (clause as Clause).head.getOrder('SPO')
-    query.instructionPtr++
+    query.pc++
   },
 
   setIndex(query: Query, index: Argument): void {
     query.dbNode = (index as Index).getOrder('SPO')
-    query.instructionPtr++
+    query.pc++
   },
 
   medialEConst(query: Query, term: Argument): void {
     const found = (query.dbNode as Branch).get(term as Term)
     if (found) {
       query.dbNode = found
-      query.instructionPtr++
+      query.pc++
     } else query.fail = true
   },
 
   medialENewVar(query: Query, term: Argument): void {
-    newEVar(query, term, advanceMedial)
+    eNewVar(query, term, advanceMedial)
   },
 
   medialEOldVar(query: Query, term: Argument): void {
-    operations.medialEConst(query, query.deref(term as Variable))
+    const found = query.deref(term as Variable)
+    if (found.termType === 'Variable') operations.medialENewVar(query, found)
+    else operations.medialEConst(query, found)
   },
 
   medialEAnonVar(query: Query, term: Argument): void {
-    anonEVar(query, advanceMedial)
+    eAnonVar(query, advanceMedial)
   },
 
   finalEConst(query: Query, term: Argument): void {
-    if ((query.dbNode as Twig).has(term as Term)) query.instructionPtr++
+    if ((query.dbNode as Twig).has(term as Term)) query.pc++
     else query.fail = true
   },
 
   finalENewVar(query: Query, term: Argument): void {
-    newEVar(query, term, advanceFinal)
+    eNewVar(query, term, advanceFinal)
   },
 
   finalEOldVar(query: Query, term: Argument): void {
-    operations.finalEConst(query, query.deref(term as Variable))
+    const found = query.deref(term as Variable)
+    if (found.termType === 'Variable') operations.finalENewVar(query, found)
+    else operations.finalEConst(query, found)
   },
 
   call(query: Query, term: Argument): void {
     query.clause = null
-    query.args = new Map()
+    query.calling = new Map()
   },
 
   emitResult(query: Query, term: Argument): void {
-    query.emit!(new Map(query.bindings))
+    query.emit!(new Map(query.scope))
     query.fail = !query.backtrack()
   },
 }

c26f435580dc06fb9eef725b5866154077e15062
Michael Pabst <michael.pabst@cs.stanford.edu>
2022-09-25 22:57:07 -0500

    Machine now has a 'heap' for variable assignments

--- src/machine.ts
+++ src/machine.ts
@@ -1,7 +1,5 @@
-import { Clause } from './clause.js'
 import { Branch, Leaf } from './operations.js'
 import { Query } from './query.js'
-import { Store } from './store.js'
 import { Term, Variable } from './term.js'
 
 export type ScopedBinding<T extends Term = Term> = [Query, T]
@@ -12,37 +10,37 @@ export type Operation = (m: Machine, l: Argument, r: Argument) => void
 export type Instruction = [Operation, Argument, Argument]
 export type Program = Instruction[]
 
-type Pending = Map<Query, ScopedBindings>
 type DBNode = Leaf | Branch
 
 class Environment {
   query: Query
   programP: number
-  scope: ScopedBindings
-  pending: Pending
-  trailP: number
+  heapP: number
+  scopeP: number
+  argsP: number
 
   constructor(machine: Machine) {
     this.query = machine.query!
     this.programP = machine.programP
-    this.scope = machine.scope!
-    this.pending = machine.pending!
-    this.trailP = machine.trailP
+    this.heapP = machine.heapP
+    this.scopeP = machine.scopeP
+    this.argsP = machine.argsP
+  }
+
+  restore(machine: Machine): void {
+    machine.query = this.query
+    machine.programP = this.programP
+    machine.heapP = this.heapP
+    machine.scopeP = this.scopeP
+    machine.argsP = this.argsP
   }
 }
 
-abstract class ChoicePoint {
-  query: Query
-  programP: number
-  scope: ScopedBindings
-  pending: Pending
+abstract class ChoicePoint extends Environment {
   trailP: number
 
-  constructor(public machine: Machine) {
-    this.query = machine.query!
-    this.programP = machine.programP
-    this.scope = machine.scope!
-    this.pending = machine.pending
+  constructor(machine: Machine) {
+    super(machine)
     this.trailP = machine.trailP
   }
 
@@ -50,14 +48,10 @@ abstract class ChoicePoint {
     return machine.query === this.query && machine.programP === this.programP
   }
 
-  restore(): void {
-    this.machine.query = this.query
-    this.machine.programP = this.programP
-    this.machine.scope = this.scope
-    // restore pending before unbind()
-    this.machine.pending = this.pending
-    while (this.machine.trailP > this.trailP) this.machine.unbind()
-    this.machine.fail = false
+  restore(machine: Machine): void {
+    super.restore(machine)
+    while (machine.trailP > this.trailP) machine.unbind()
+    machine.fail = false
   }
 }
 
@@ -69,13 +63,13 @@ class IteratingChoicePoint extends ChoicePoint {
     this.iterator = machine.dbNode!.keys()
   }
 
-  next(): IteratorResult<Term> {
+  next(machine: Machine): IteratorResult<Term> {
     const out = this.iterator.next()
     // save an iteration by checking IteratorHasMore instead
     // of waiting for done?
     if (out.done) {
-      this.machine.orP--
-      this.machine.fail = true
+      machine.orP--
+      machine.fail = true
     }
     return out
   }
@@ -93,23 +87,26 @@ export class Machine {
   // check for negation?
 
   /// Global stuff
-  stack: ChoicePoint[] = []
+  stack: Environment[] = []
   andP: number = -1
   orP: number = -1
 
-  emit: ((b: ScopedBindings) => void) | null = null
+  emit: ((b: Bindings) => void) | null = null
 
   /// Environment stuff
   query: Query | null = null
   programP: number = -1
 
-  scopeP: number = -1
   dbNode: DBNode | null = null
 
-  // address in stack of where current callee's args begin
-  argsP: number = -1
+  // abuse of terminology: not really a heap, just sorta
+  // like the WAM's heap
+  heap: (Term | number)[] = []
+  heapP: number = -1 // start of environment
+  scopeP: number = 0 // start of our args, in prev env
+  argsP: number = -1 // start of current callee's args
 
-  trail: ScopedBinding<Variable>[] = []
+  trail: number[] = []
   trailP: number = -1
 
   /// Instruction-local stuff
@@ -118,51 +115,58 @@ export class Machine {
   // maybe: set fail, fetch next instr, if 'not', then continue, else backtrack
   fail: boolean = false
 
-  constructor(public store: Store) {}
-
   backtrack(): boolean {
     if (this.orP < 0) return false
-    this.stack[this.orP].restore()
+    this.stack[this.orP].restore(this)
     return true
   }
 
   // todo: avoid branching in deref() and bind() by also having special-case
   // implementations for this.scope and this.calleeArgs
-  deref([query, variable]: ScopedBinding<Variable>): ScopedBinding {
-    const found =
-      query === this.query
-        ? this.scope!.get(variable)!
-        : this.pending.get(query)!.get(variable)!
-    if (found[1] instanceof Variable)
-      return found[1] === variable
-        ? found
-        : this.deref(found as ScopedBinding<Variable>)
-    else return found
+  deref(addr: number): Term | number {
+    while (true) {
+      const found = this.heap[addr]
+      if (typeof found === 'number' && found !== addr) addr = found
+      else return found
+    }
   }
 
-  bind(vari: ScopedBinding<Variable>, value: ScopedBinding): void {
-    if (vari[0] === this.query) this.scope!.set(vari[1], value)
-    else this.pending.get(vari[0]!)!.set(vari[1], value)!
+  bind(addr: number, value: Term | number): void {
+    this.heap[addr] = value
     this.trailP++
-    this.trail[this.trailP] = vari
+    this.trail[this.trailP] = addr
   }
 
   unbind(): void {
     const binding = this.trail[this.trailP]
-    if (binding[0] === this.query) this.scope!.set(binding[1], binding)
-    else this.pending.get(binding[0])!.set(binding[1], binding)
+    this.heap[binding] = binding
     this.trailP--
+  }
+
+  initArgs(args: Bindings): void {
+    for (let prev of this.query!.vars) {
+      let next: Term | undefined
+      while (true) {
+        next = args.get(prev)
+        if (!(next instanceof Variable)) break
+        prev = next
+      }
+      if (!next) this.heap.push(this.query!.vars.indexOf(prev))
+      else this.heap.push(next)
+    }
+    this.heapP = this.heap.length
   }
 
   evaluate(
     query: Query,
-    emit: (b: ScopedBindings) => void = console.log,
+    emit: (b: Bindings) => void = console.log,
     args: Bindings = new Map(),
   ): void {
-    this.emit = emit
-    this.scope = query.newScope(args)
+    this.query = query
     this.programP = 0
+    this.initArgs(args)
     this.fail = false
+    this.emit = emit
     while (true) {
       const [op, left, right] = query.program[this.programP]
       op(this, left, right)
@@ -173,11 +177,11 @@ export class Machine {
   nextChoice(): IteratorResult<Term> {
     let cp
     if (this.orP > -1) cp = this.stack[this.orP] as IteratingChoicePoint
-    if (!cp?.isCurrent(this)) {
+    if (!cp || !cp.isCurrent(this)) {
       cp = new IteratingChoicePoint(this)
       this.orP = Math.max(this.andP, this.orP) + 1
       this.stack[this.orP] = cp
     }
-    return cp.next()
+    return cp.next(this)
   }
 }

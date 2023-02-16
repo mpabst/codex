c5d00be85691dabc6952d3f8ea3853d552e114aa
Michael Pabst <michael.pabst@cs.stanford.edu>
2023-02-15 23:10:29 -0600

    Importing stuff with fp-test

--- src/test/append.ts
+++ src/test/append.ts
@@ -1,14 +1,29 @@
-import { LitElement, html, css } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { until } from 'lit-html/directives/until.js'
+import { customElement } from 'lit/decorators.js'
+import { css, html, LitElement } from 'lit/index.js'
+
+import { Prefixers } from '../data-factory.js'
+import { Store } from '../store.js'
+
+const { test } = Prefixers
 
 @customElement('fp-test')
 export class Test extends LitElement {
-  static styles = css`h1 { font-style: italic; }`
+  static styles = css`
+    h1 {
+      font-style: italic;
+    }
+  `
 
-  @property()
-  text = ''
+  store = new Store()
+  node = test('append')
+
+  async facts() {
+    await this.store.load(this.node)
+    return this.store.modules.get(this.node)!.facts
+  }
 
   render() {
-    return html`<h1>${this.text}</h1>`
+    return until(this.facts().then(f => html`${f.getRoot('SPO').size}`))
   }
 }
